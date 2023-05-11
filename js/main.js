b05e9f9fbd1657971e7503ce162b95c14d1c5a1c
Zudikhin <uzudihin@gmail.com>
2023-05-11 16:22:39 +0300

    home page

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    "use strict";
+
+	$(".faq_block_left_item").click(function() {
+		$(this).find(".faq_block_left_item_body").slideToggle();
+	});
+
+	// $(".header_block_btn").click(function() {
+	// 	$(this).toggleClass("active");
+	// 	$(".header_drop").slideToggle();
+	// 	$(".main").toggleClass("open");
+	// 	$(".about_main").toggleClass("open");
+	// 	$(".properties_single").toggleClass("open");
+	// });
+
+});
