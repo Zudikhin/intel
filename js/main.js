f37288146ae807cc8279c0cdd3078bb20f5e8c96
Zudikhin <uzudihin@gmail.com>
2023-05-16 20:21:56 +0300

    2 pages

--- js/main.js
+++ js/main.js
@@ -5,12 +5,13 @@ $(document).ready(function () {
 		$(this).find(".faq_block_left_item_body").slideToggle();
 	});
 
-	// $(".header_block_btn").click(function() {
-	// 	$(this).toggleClass("active");
-	// 	$(".header_drop").slideToggle();
-	// 	$(".main").toggleClass("open");
-	// 	$(".about_main").toggleClass("open");
-	// 	$(".properties_single").toggleClass("open");
-	// });
+
+	$(".header_block_btn").click(function() {
+		$(this).toggleClass("active");
+		$(".header_drop").slideToggle();
+		$(".main").toggleClass("open");
+		$(".about_main").toggleClass("open");
+		$(".properties_single").toggleClass("open");
+	});
 
 });
